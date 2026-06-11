efd99d23f759674cb4a463e11cf66669ef1b50b8
Rajkutty <ramakrishnanrajadurai464@gmail.com>
2026-06-11 12:31:11 +0530

    coolw

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
     VitePWA({
       registerType: 'autoUpdate',
       manifest: {
@@ -28,5 +31,5 @@ export default defineConfig({
         ]
       }
     })
-  ]
+  ],
 })
